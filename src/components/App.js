061bbdce88a386c43c43c08ebed19a686c14ea56
Apesosmarc <73441355+Apesosmarc@users.noreply.github.com>
2021-09-26 20:54:55 -0400

    Added loading spinner

--- src/components/App.js
+++ src/components/App.js
@@ -13,23 +13,25 @@ export default class App extends Component {
     team: teams[this.props.id],
     standings: [],
     nextGame: [],
-    active: "false",
     schedule: [],
     division: [],
+    isLoading: true,
   };
 
   getData = async () => {
+    console.log(this.state.team.mainColor);
+    this.setState({ isLoading: true });
     console.log("fetch");
     const standings = await divisionStandings.get("/standings");
     const nextGame = await game("next").get(`/teams/${this.state.teamId}`);
     const getSchedule = await gamesList(this.state.teamId).get("/schedule");
 
+    setTimeout(() => this.setState({ isLoading: false }), 800);
     this.setState({
       team: teams[this.state.teamId],
       division: standings.data.records[this.state.team.covidDiv].division.name,
       standings: standings.data.records[this.state.team.covidDiv].teamRecords,
       nextGame: nextGame.data.teams[0],
-      active: isLive(nextGame) === true ? true : false,
       schedule: getSchedule.data,
     });
   };
@@ -47,7 +49,7 @@ export default class App extends Component {
   };
 
   render() {
-    return (
+    return this.state.isLoading === false ? (
       <div className="container max-w-lg sm:max-w-xl md:max-w-2xl lg:max-w-4xl mx-auto">
         <div>
           <TeamText team={this.state.team} />
@@ -70,7 +72,9 @@ export default class App extends Component {
           <div className="pt-16 pb-8 flex content-center justify-center">
             <h1
               className="bottom-header sm:text-4xl"
-              style={{ borderBottom: `3px solid ${this.state.team.mainColor}` }}
+              style={{
+                borderBottom: `3px solid ${this.state.team.mainColor}`,
+              }}
             >
               {this.state.team.name}
             </h1>
@@ -78,6 +82,24 @@ export default class App extends Component {
 
           <Stats team={this.state.team} />
         </div>
+      </div>
+    ) : (
+      <div className="container max-w-lg sm:max-w-xl md:max-w-2xl lg:max-w-4xl mx-auto">
+        <svg class="animate-spin h-10 w-8" viewBox="0 0 24 24">
+          <circle
+            class="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="gray"
+            stroke-width="4"
+          ></circle>
+          <path
+            class="opacity-75"
+            fill="white"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+          ></path>
+        </svg>
       </div>
     );
   }
